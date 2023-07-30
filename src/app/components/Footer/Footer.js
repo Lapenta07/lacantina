f5be70932cf251e24fc3a87a5cda766775099e39
Ignacio Lapenta <ignaciolapenta@gmail.com>
2023-07-29 23:18:44 -0300

    facha

--- src/app/components/Footer/Footer.js
+++ src/app/components/Footer/Footer.js
@@ -5,6 +5,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCartShopping } from "@fortawesome/free-solid-svg-icons";
 
 
+
 export default function Footer (){
     return(
         <footer className="footer">
